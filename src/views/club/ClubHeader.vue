adb9b2e3a8f38228bdbcfac029bfed42771be36f
pserimer <p_serimer@outlook.com>
2022-09-23 10:45:23 +0300

    Club Page

---
+++ src/views/club/ClubHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar class="primary" dense >
+    <img class="mr-3" :src="require('@/assets/logo.png')" height="40" style="border-radius: 8px"/>
+    <v-toolbar-title>
+      Sports Management Project
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+    <v-btn @click="logout">
+      Çıkış yap
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      this.$router.push("/")
+    }
+  }
+}
+</script>
---
+++ src/views/club/ClubPanel.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <ClubHeader style="margin-bottom: 2%"/>
+    <div style="margin-bottom: 2%">
+      <center>
+        <v-img
+            src="@/assets/anadolu-efes-logo.png"
+            max-height="100px"
+            max-width="75px"
+        ></v-img>
+      </center>
+    </div>
+    <v-container class="primary rounded" >
+      <v-layout wrap>
+        <v-flex xs10 md>
+          <v-row height="200px">
+            <v-col>
+              <h1 class="font-italic"> Hoşgeldin {{ user }}! </h1>
+              <span class="text--secondary font-weight-medium"> Ankara - {{ getDate }} </span>
+            </v-col>
+          </v-row>
+
+          <v-row style="margin-left: 40%">
+            <h2 class="font-weight-light"> Bugün hangi işlemi yapmak istersin? </h2>
+          </v-row>
+
+          <v-row height="200px">
+            <v-col style="margin-left: 10%; padding-left: 7%">
+              <v-card class="ma-2 secondary" shaped width="340px">
+                <v-card-title class="justify-center">
+                  <router-link :to=branchPath style="text-decoration:none; color: white">
+                    <span> Kulüp şubelerini incele </span>
+                  </router-link>
+                </v-card-title>
+              </v-card>
+            </v-col>
+
+            <v-col>
+              <v-card class="ma-2 secondary" shaped width="340px">
+                <v-card-title class="justify-center">
+                  <router-link :to=coordinatorPath style="text-decoration:none; color: white">
+                    <span> Kulüp koordinatörlerini incele </span>
+                  </router-link>
+                </v-card-title>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+import ClubHeader from "@/views/club/ClubHeader";
+
+export default {
+  components: {
+    ClubHeader
+  },
+  data() {
+    return {
+      user: "logged userName",
+      clubid: 1
+    }
+  },
+  methods: {},
+  computed: {
+    getDate() {
+      return moment().seconds(0).milliseconds(0);
+    },
+    branchPath() {
+      return '/club/' + this.clubid + '/branches'
+    },
+    coordinatorPath() {
+      return '/club/' + this.clubid + '/coordinators'
+    }
+  }
+}
+</script>
